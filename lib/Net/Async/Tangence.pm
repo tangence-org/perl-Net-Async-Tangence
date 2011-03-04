eb6ea7528d47599e3084effe727e2a9e415acfca
Paul "LeoNerd" Evans <leonerd@leonerd.org.uk>
2011-03-04 19:59:11 +0000

    Added minimal Net::Async::Tangence file to keep M::B happy

---
+++ lib/Net/Async/Tangence.pm
@@ -0,0 +1,24 @@
+#  You may distribute under the terms of either the GNU General Public License
+#  or the Artistic License (the same terms as Perl itself)
+#
+#  (C) Paul Evans, 2011 -- leonerd@leonerd.org.uk
+
+package Net::Async::Tangence;
+
+use strict;
+use warnings;
+
+our $VERSION = '0.04';
+
+=head1 NAME
+
+C<Net::Async::Tangence> - use L<Tangence> with L<IO::Async>
+
+=cut
+
+# Keep perl happy; keep Britain tidy
+1;
+
+=head1 AUTHOR
+
+Paul Evans <leonerd@leonerd.org.uk>
