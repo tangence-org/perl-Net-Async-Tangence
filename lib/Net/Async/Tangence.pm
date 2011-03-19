c30e8e63a9a3739f3f6a1b270e603ff36d92c89a
Paul "LeoNerd" Evans <leonerd@leonerd.org.uk>
2011-03-19 20:02:05 +0000

    Provide some top-level module documentation

--- lib/Net/Async/Tangence.pm
+++ lib/Net/Async/Tangence.pm
@@ -12,9 +12,32 @@ our $VERSION = '0.04';
 
 =head1 NAME
 
-C<Net::Async::Tangence> - use L<Tangence> with L<IO::Async>
+C<Net::Async::Tangence> - use C<Tangence> with C<IO::Async>
+
+=head1 DESCRIPTION
+
+This distribution provides concrete implementations of the L<Tangence> base
+classes, allowing either servers or clients to be written based on
+L<IO::Async>.
+
+To implement a server, see L<Net::Async::Tangence::Server>.
+
+To implement a client, see L<Net::Async::Tangence::Client>.
+
+This module itself does not provide any code, and exists only to provide the
+module C<$VERSION> and top-level documentation.
 
 =cut
+
+=head1 SEE ALSO
+
+=over 8
+
+=item *
+
+L<Tangence> - attribute-oriented server/client object remoting framework
+
+=back
 
 =head1 AUTHOR
 
